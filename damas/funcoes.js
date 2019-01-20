6e5fa971d13a76a6c7829f26ac69d37d8ac7b97a
Cleber Tomazoni <clebertomazoni@hotmail.com>
2019-01-20 16:30:44 -0200

    Damas - Adicionado evento de seleção das peças

--- damas/funcoes.js
+++ damas/funcoes.js
@@ -1,3 +1,5 @@
+var pecaAnterior;
+var pecaSelecionada;
 function mover(origem, destino, peca) {
 	var cor = 'branca';
 	if (peca.startsWith('p')) {
@@ -5,4 +7,17 @@ function mover(origem, destino, peca) {
 	}
 	document.getElementById(origem).innerHTML = '';
 	document.getElementById(destino).innerHTML = '<div class="peca ' + cor + '" id="' + peca + '"></div>';
+}
+function selecionar() {
+	pecaAnterior = pecaSelecionada;
+	pecaSelecionada = this.id;
+	document.querySelector('#'+pecaSelecionada).classList.toggle('selecionada');
+}
+function adicionarEventos() {
+	for (var i = 1; i <= 12; i++) {
+		var branca = 'b'+i;
+		var preta = 'p'+i;
+		document.getElementById(preta).addEventListener('click',selecionar);
+		document.getElementById(branca).onclick = selecionar;
+	}
 }
