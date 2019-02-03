3c4d6e22d158488644355a3e91a32a36d754e6dd
Cleber Tomazoni <clebertomazoni@hotmail.com>
2019-02-03 17:28:00 -0200

    Damas - Jogo parcialmente completo

--- damas/funcoes.js
+++ damas/funcoes.js
@@ -5,17 +5,51 @@ function Peca(id, pos) {
 	this.dama = false,
 	this.promocao = function() { this.dama = true }
 }
+var vez;
 var pecas = [];
-var pecaAnterior;
-var pecaSelecionada;
+var captP = [];
+var captC = [];
+var pecaSelecionada = '';
 var casasSelecionadas = [];
+var qtdPecasBrancas = 12;
+var qtdPecasPretas = 12;
 function mover(peca, destino) {
 	document.getElementById(peca.posicao).innerHTML = '';
 	document.getElementById(destino).innerHTML = '<div class="peca ' + peca.cor + '" id="' + peca.id + '"></div>';
 	peca.posicao = destino;
+	if (!peca.dama) {
+		var pos = obterNumero(destino);
+		if (pos < 9 || pos > 56) {
+			peca.promocao();
+		}
+	}
+	if (peca.cor == 'branca') {
+		vez = 'p';
+	} else {
+		vez = 'b';
+	}
+	atualizarPlacar('info');
+	if (document.getElementById(destino).classList.contains('capt')) {
+		removerPecaTabuleiro(destino);
+	}
+	desmarcarPossiveisCapturas();
 }
-function removerPecaTabuleiro(peca) {
+function removerPecaTabuleiro(pos) {
+	var codPecaCapt;
+	for (var i = 0; i <= captC.length; i++) {
+		if (captC[i] == pos) {
+			codPecaCapt = i;
+			break;
+		}
+	}
+	var peca = captP[codPecaCapt];
 	document.getElementById(peca.posicao).innerHTML = '';
+	if (peca.cor == 'branca') {
+		qtdPecasBrancas--;
+	} else {
+		qtdPecasPretas--;
+	}
+	atualizarPlacar('pont');
 }
 function obterNumero(valor) {
 	return parseInt(valor.split(valor.charAt(0))[1]);
@@ -49,16 +83,38 @@ function obterSePecaPosicao(pos) {
 function obterIdPecaPosicao(pos) {
 	return document.getElementById(pos).innerHTML.split('"')[3];
 }
+function desmarcarCasasSelecionadas() {
+	while (casasSelecionadas.length > 0) {
+		document.getElementById(casasSelecionadas.pop()).classList.remove('c-selecionada');
+	}
+}
+function marcarPossivelCaptura(peca, casa) {
+	captP.push(peca);
+	captC.push(casa);
+	document.getElementById(casa).classList.add('capt');
+}
+function desmarcarPossiveisCapturas() {
+	while (captC.length > 0) {
+		document.getElementById(captC.pop()).classList.remove('capt');
+		captP = [];
+	}
+}
 function selecionarP(p) {
-	if (pecaSelecionada == undefined) {
+	if (pecaSelecionada == '') {
 		pecaSelecionada = p;
 	}
 	if (pecaSelecionada != p) {
-		pecaAnterior = pecaSelecionada;
+		var pecaAnterior = pecaSelecionada;
 		pecaSelecionada = p;
 		document.querySelector('#'+pecaAnterior).classList.remove('p-selecionada');
 		document.querySelector('#'+pecaSelecionada).classList.add('p-selecionada');
+		selecionarC(obterObjPeca(p).posicao);
 	} else {
+		if (document.querySelector('#'+p).classList.contains('p-selecionada')) {
+			pecaSelecionada = '';
+		} else {
+			selecionarC(obterObjPeca(p).posicao);
+		}
 		document.querySelector('#'+p).classList.toggle('p-selecionada');
 	}
 }
@@ -66,45 +122,100 @@ function selecionarC(c) {
 	var peca = obterObjPeca(obterIdPecaPosicao(c));
 	var coord = obterCoordPosicao(peca);
 	if (coord.coluna > 1 && coord.linha < 8 && (peca.cor == 'branca' || peca.dama)) {
-		numCasa = coord.numPos+7;
-		if (!obterSePecaPosicao('c'+numCasa)) {
-			document.querySelector('#c'+numCasa).classList.toggle('c-selecionada');
-			casasSelecionadas.push(numCasa);
+		var casa = 'c'+(coord.numPos+7);
+		if (!obterSePecaPosicao(casa)) {
+			casasSelecionadas.push(casa);
 		} else {
-			var peca2 = obterObjPeca(obterIdPecaPosicao('c'+numCasa));
+			var peca2 = obterObjPeca(obterIdPecaPosicao(casa));
 			var coord2 = obterCoordPosicao(peca2);
-			if (coord2.coluna > 1 && coord2.linha < 8 && (peca.cor == 'branca' || peca.dama)) {
-				numCasa = coord2.numPos+7;
-				if (!obterSePecaPosicao('c'+numCasa) && peca2.cor == 'preta') {
-					document.querySelector('#c'+numCasa).classList.toggle('c-selecionada');
-					casasSelecionadas.push(numCasa);
+			if (coord2.coluna > 1 && coord2.linha < 8) {
+				casa = 'c'+(coord2.numPos+7);
+				if (!obterSePecaPosicao(casa) && peca.cor != peca2.cor) {
+					casasSelecionadas.push(casa);
+					marcarPossivelCaptura(peca2, casa);
 				}
 			}
 		}
 	}
 	if (coord.coluna < 8 && coord.linha < 8 && (peca.cor == 'branca' || peca.dama)) {
-		numCasa = coord.numPos+9;
-		document.querySelector('#c'+numCasa).classList.toggle('c-selecionada');
+		var casa = 'c'+(coord.numPos+9);
+		if (!obterSePecaPosicao(casa)) {
+			casasSelecionadas.push(casa);
+		} else {
+			var peca2 = obterObjPeca(obterIdPecaPosicao(casa));
+			var coord2 = obterCoordPosicao(peca2);
+			if (coord2.coluna < 8 && coord2.linha < 8) {
+				casa = 'c'+(coord2.numPos+9);
+				if (!obterSePecaPosicao(casa) && peca.cor != peca2.cor) {
+					casasSelecionadas.push(casa);
+					marcarPossivelCaptura(peca2, casa);
+				}
+			}
+		}
 	}
 	if (coord.coluna > 1 && coord.linha > 1 && (peca.cor == 'preta' || peca.dama)) {
-		numCasa = coord.numPos-9;
-		document.querySelector('#c'+numCasa).classList.toggle('c-selecionada');
+		var casa = 'c'+(coord.numPos-9);
+		if (!obterSePecaPosicao(casa)) {
+			casasSelecionadas.push(casa);
+		} else {
+			var peca2 = obterObjPeca(obterIdPecaPosicao(casa));
+			var coord2 = obterCoordPosicao(peca2);
+			if (coord2.coluna > 1 && coord2.linha > 1) {
+				casa = 'c'+(coord2.numPos-9);
+				if (!obterSePecaPosicao(casa) && peca.cor != peca2.cor) {
+					casasSelecionadas.push(casa);
+					marcarPossivelCaptura(peca2, casa);
+				}
+			}
+		}
 	}
 	if (coord.coluna < 8 && coord.linha > 1 && (peca.cor == 'preta' || peca.dama)) {
-		numCasa = coord.numPos-7;
-		document.querySelector('#c'+numCasa).classList.toggle('c-selecionada');
+		var casa = 'c'+(coord.numPos-7);
+		if (!obterSePecaPosicao(casa)) {
+			casasSelecionadas.push(casa);
+		} else {
+			var peca2 = obterObjPeca(obterIdPecaPosicao(casa));
+			var coord2 = obterCoordPosicao(peca2);
+			if (coord2.coluna < 8 && coord2.linha > 1) {
+				casa = 'c'+(coord2.numPos-7);
+				if (!obterSePecaPosicao(casa) && peca.cor != peca2.cor) {
+					casasSelecionadas.push(casa);
+					marcarPossivelCaptura(peca2, casa);
+				}
+			}
+		}
+	}
+	for (var i = 0; i < casasSelecionadas.length; i++) {
+		document.getElementById(casasSelecionadas[i]).classList.toggle('c-selecionada');
 	}
 }
 function selecionar() {
 	if (obterSePecaPosicao(this.id)) {
+		desmarcarCasasSelecionadas();
 		selecionarP(obterIdPecaPosicao(this.id));
-		selecionarC(this.id);
 	} else {
 		if (document.getElementById(this.id).classList.contains('c-selecionada')) {
 			mover(obterObjPeca(pecaSelecionada), this.id);
-			while (casasSelecionadas.length > 0) {
-				document.querySelector('#c'+casasSelecionadas.pop()).classList.remove('c-selecionada');
-			}
+			desmarcarCasasSelecionadas();
+		}
+	}
+}
+function atualizarPlacar(cod) {
+	if (cod == 'info') {
+		document.getElementById('info').innerHTML = 'Vez de '+document.getElementById(vez == 'b' ? 'djb' : 'djp').innerHTML+'.';
+	} else if (cod == 'pont') {
+		document.getElementById('pjb').innerHTML = 12 - qtdPecasPretas;
+		document.getElementById('pjp').innerHTML = 12 - qtdPecasBrancas;
+		if (qtdPecasPretas == 0) {
+			document.getElementById('info').innerHTML = document.getElementById('djb').innerHTML+' venceu a partida.';
+		} else if (qtdPecasBrancas == 0) {
+			document.getElementById('info').innerHTML = document.getElementById('djp').innerHTML+' venceu a partida.';
+		}
+	} else if (cod == 'desc') {
+		document.getElementById('djb').innerHTML = 'J1';
+		document.getElementById('djp').innerHTML = 'J2';
+		if (vez != undefined) {
+			atualizarPlacar('info');
 		}
 	}
 }
